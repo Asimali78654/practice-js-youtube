abaa85ec039d1450bc335cb0086d3c87f6fd7531
Asimali78654 <135886965+Asimali78654@users.noreply.github.com>
2023-10-22 02:12:54 +0000

    datatypes

---
+++ 01-bacis/variable.js
@@ -0,0 +1,18 @@
+// const employeeId=12345
+// let employeeEmail="malikasimali111@gmail.com"
+// var accountPass="123njdn"
+// accountCity="Karachi"
+// employeeEmail="asim4y74y83y8"
+// accountPass="798"
+// accountCity="Lahore"
+// let accountState 
+// console.table([employeeEmail,employeeId,accountPass,accountCity,accountState],typeof employeeEmail)
+let email="nwhdhqwheehdjeod"
+let age=12
+console.log(age);
+console.log(email)
+console.log(typeof age)
+console.log(typeof email)
+let depart=null
+console.log(typeof null)
+console.log(typeof undefined)
